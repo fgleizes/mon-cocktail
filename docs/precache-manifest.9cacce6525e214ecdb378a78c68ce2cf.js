3d3b07682022159526632e2a4e92a1be89c7717c
benjamin <benj.habert@gmail.com>
2020-11-25 15:17:31 +0100

    deploy website to github-pages

---
+++ docs/precache-manifest.9cacce6525e214ecdb378a78c68ce2cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff5f90e2bd8c97a9d660d692a60a34c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75353f110a8139dbf3df",
+    "url": "./static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "50b81fd2cb3e7ed5b988",
+    "url": "./static/css/main.df5a8458.chunk.css"
+  },
+  {
+    "revision": "75353f110a8139dbf3df",
+    "url": "./static/js/2.62fc2bd8.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "./static/js/2.62fc2bd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b81fd2cb3e7ed5b988",
+    "url": "./static/js/main.46489f5d.chunk.js"
+  },
+  {
+    "revision": "883ab4936dde21b532ee",
+    "url": "./static/js/runtime-main.2eac4daf.js"
+  }
+]);
